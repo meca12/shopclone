d7202de0bb2b141030731e5c3134199afee589c6
Martha  avila <itit.martha.avil@gmail.com>
2024-12-29 21:40:25 -0600

    routing-home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { ContactComponent } from './componentes/contact/contact.component';
-import { HomeComponent } from './componentes/home/home.component';
 import { NgModule } from '@angular/core';
 import { SearchComponent } from './componentes/search/search.component';
 import { ShopComponent } from './componentes/shop/shop.component';
@@ -12,7 +11,6 @@ import { SingComponent } from './componentes/sing/sing.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     AboutComponent,
     ShopComponent,
     ContactComponent,
